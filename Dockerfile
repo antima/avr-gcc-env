74dafe43d007992a3f615136f5b6c9eaf25f6c85
mar <gim.marcello@gmail.com>
2022-07-24 19:42:49 +0200

    added: the building of the toolchain gets executed at runtime instead that at build time; build instructions moved to the entrypoint.sh script.

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,21 @@
 FROM debian:bullseye-slim
 
+ARG BINUTILS_VERSION=""
+ARG LIBC_VERSION=""
+ARG GCC_VERSION=""
+
 ENV PREFIX="$HOME/local/avr"
 ENV PATH="$PATH:$PREFIX/bin"
 
+ENV BINUTILS_VERSION=${BINUTILS_VERSION}
+ENV LIBC_VERSION=${LIBC_VERSION}
+ENV GCC_VERSION=${GCC_VERSION}
+
 WORKDIR /base
+COPY entrypoint.sh .
 
 RUN apt-get -y update && apt-get -y install build-essential curl git \
-    autotools-dev autoconf python3 python-is-python3 texinfo
+    autotools-dev autoconf python3 python-is-python3 texinfo && \ 
+		chmod +x entrypoint.sh
 
-RUN curl --limit-rate 1G -o binutils.tar.gz -L https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.gz && tar -xf binutils.tar.gz && \
-    curl --limit-rate 1G -o gcc.tar.gz -L https://ftp.gnu.org/gnu/gcc/gcc-11.3.0/gcc-11.3.0.tar.gz && tar -xf gcc.tar.gz && \
-    git clone https://github.com/avrdudes/avr-libc
-
-# compile binutils first
-WORKDIR /base/binutils-2.38/obj-avr
-RUN ../configure --prefix=$PREFIX --target=avr --disable-nls && make -j$(nproc) && make install
-
-# then gcc with avr as a target
-WORKDIR /base/gcc-11.3.0
-RUN ./contrib/download_prerequisites
-
-WORKDIR /base/gcc-11.3.0/obj-avr
-RUN ../configure --prefix=$PREFIX --target=avr --enable-languages=c,c++ \
-    --disable-nls --disable-libssp --with-dwarf2 && make -j$(nproc) && make install
-
-# and finally libc
-WORKDIR /base/avr-libc
-RUN ./bootstrap && ./configure --prefix=$PREFIX --build=`./config.guess` --host=avr && make \
-    -j$(nproc) && make install
-
-CMD []
+ENTRYPOINT ["./entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# pass the versions for binutils, gcc and libc
+[ -z "$BINUTILS_VERSION" ] && (echo "BINUTILS_VERSION is not defined"; exit 1;)
+[ -z "$LIBC_VERSION" ] && (echo "LIBC_VERSION is not defined"; exit 1;) 
+[ -z "$GCC_VERSION" ] && (echo "GCC_VERSION is not defined"; exit 1;)
+
+# libc git tags uses '_' as version delimiter
+LIBC_VERSION=${LIBC_VERSION//./_}
+
+printf "binutils:%s\n" "$BINUTILS_VERSION"
+printf "gcc:%s\n" "$GCC_VERSION"
+printf "libc:%s\n" "$LIBC_VERSION"
+
+
+BINUTILS_OBJ_DIR="/base/binutils-$BINUTILS_VERSION/obj-avr"
+GCC_BASE_DIR="/base/gcc-$GCC_VERSION"
+GCC_OBJ_DIR="/base/gcc-$GCC_VERSION/obj-avr"
+LIBC_OBJ_DIR="/base/avr-libc"
+
+# download sources
+curl --limit-rate 1G -o binutils.tar.gz -L "https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.gz" && tar -xf binutils.tar.gz && \
+curl --limit-rate 1G -o gcc.tar.gz -L "https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz" && tar -xf gcc.tar.gz && \
+git clone https://github.com/avrdudes/avr-libc --branch avr-libc-"$LIBC_VERSION"-release
+
+# compile binutils first
+mkdir -p "$BINUTILS_OBJ_DIR"
+cd "$BINUTILS_OBJ_DIR" || (echo "could not find dir for binutils"; exit 1;) 
+../configure --prefix="$PREFIX" --target=avr --disable-nls && make -j"$(nproc)" && make install
+
+# then gcc with avr as a target
+cd "$GCC_BASE_DIR" || (echo "could not find dir for gcc"; exit 1;) 
+./contrib/download_prerequisites
+
+mkdir -p "$GCC_OBJ_DIR"
+cd "$GCC_OBJ_DIR" || (echo "could not find dir for libc"; exit 1;) 
+../configure --prefix="$PREFIX" --target=avr --enable-languages=c,c++ \
+--disable-nls --disable-libssp --with-dwarf2 && make -j"$(nproc)" && make install
+
+# and finally libc
+mkdir -p "$LIBC_OBJ_DIR"
+cd "$LIBC_OBJ_DIR" || (echo "could not find dir for libc"; exit 1;)
+./bootstrap && ./configure --prefix="$PREFIX" --build="$(./config.guess)" --host=avr && make \
+-j"$(nproc)" && make install
