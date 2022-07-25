e989741701e34801aed296e53a15bb8f2ea2a4bf
mar <gim.marcello@gmail.com>
2022-07-25 23:16:49 +0200

    changed: fixed typos, formatting.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@
 # libc git tags uses '_' as version delimiter
 LIBC_VERSION=${LIBC_VERSION//./_}
 
-printf "binutils:%s\n" "$BINUTILS_VERSION"
-printf "gcc:%s\n" "$GCC_VERSION"
-printf "libc:%s\n" "$LIBC_VERSION"
+printf "binutils: %s\n" "$BINUTILS_VERSION"
+printf "gcc: %s\n" "$GCC_VERSION"
+printf "libc: %s\n" "$LIBC_VERSION"
 
 
 BINUTILS_OBJ_DIR="/base/binutils-$BINUTILS_VERSION/obj-avr"
@@ -25,20 +25,20 @@ git clone https://github.com/avrdudes/avr-libc --branch avr-libc-"$LIBC_VERSION"
 
 # compile binutils first
 mkdir -p "$BINUTILS_OBJ_DIR"
-cd "$BINUTILS_OBJ_DIR" || (echo "could not find dir for binutils"; exit 1;) 
+cd "$BINUTILS_OBJ_DIR" || (echo "could not find the obj dir for binutils"; exit 1;) 
 ../configure --prefix="$PREFIX" --target=avr --disable-nls && make -j"$(nproc)" && make install
 
 # then gcc with avr as a target
-cd "$GCC_BASE_DIR" || (echo "could not find dir for gcc"; exit 1;) 
+cd "$GCC_BASE_DIR" || (echo "could not find the base dir for gcc"; exit 1;) 
 ./contrib/download_prerequisites
 
 mkdir -p "$GCC_OBJ_DIR"
-cd "$GCC_OBJ_DIR" || (echo "could not find dir for libc"; exit 1;) 
+cd "$GCC_OBJ_DIR" || (echo "could not find the obj dir for gcc"; exit 1;) 
 ../configure --prefix="$PREFIX" --target=avr --enable-languages=c,c++ \
 --disable-nls --disable-libssp --with-dwarf2 && make -j"$(nproc)" && make install
 
 # and finally libc
 mkdir -p "$LIBC_OBJ_DIR"
-cd "$LIBC_OBJ_DIR" || (echo "could not find dir for libc"; exit 1;)
+cd "$LIBC_OBJ_DIR" || (echo "could not find the obj dir for libc"; exit 1;)
 ./bootstrap && ./configure --prefix="$PREFIX" --build="$(./config.guess)" --host=avr && make \
 -j"$(nproc)" && make install
